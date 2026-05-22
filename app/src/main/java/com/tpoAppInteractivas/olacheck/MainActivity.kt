0a0c29cbc3087eb44c525fcc44024ad86c991444
Lucasdv96 <ludelvalle@uade.edu.ar>
2026-05-22 20:58:36 -0300

    Update MainActivity.kt

--- app/src/main/java/com/tpoAppInteractivas/olacheck/MainActivity.kt
+++ app/src/main/java/com/tpoAppInteractivas/olacheck/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tpoAppInteractivas.olacheck.ui.theme.OlaCheckTheme
 import com.tpoAppInteractivas.olacheck.ui.navigation.NavGraph
+import com.tpoAppInteractivas.olacheck.ui.theme.OlaCheckTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +19,5 @@ class MainActivity : ComponentActivity() {
                 NavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OlaCheckTheme {
-        Greeting("Android")
     }
 }
