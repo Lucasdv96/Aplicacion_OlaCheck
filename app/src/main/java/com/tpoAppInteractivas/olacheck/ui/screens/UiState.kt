56818691dbcbccd0aa8ceb09fbbd9dbde7ccd175
Lucasdv96 <ludelvalle@uade.edu.ar>
2026-05-26 23:23:16 -0300

    /error no puedo sincronizar el gradle

--- app/src/main/java/com/tpoAppInteractivas/olacheck/ui/screens/UiState.kt
+++ app/src/main/java/com/tpoAppInteractivas/olacheck/ui/screens/UiState.kt
@@ -3,8 +3,8 @@ package com.tpoAppInteractivas.olacheck.ui.screens
 import perfetto.protos.UiState
 
 sealed class UiState<out T>{
-    object Loading: UiState<Nothing>()
-    data class Success<T> (val data: T) : UiState<T>()
-    data class Error (val message: String) : UiState<Nothing>()
-    object Offline : UiState<Nothing>()
+    class Loading<T> : UiState<T>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error<T>(val message: String) : UiState<T>()
+    class Offline<T> : UiState<T>()
 }
